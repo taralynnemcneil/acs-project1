f478d5f9f5b139f7d917571734e17dea808b5b53
Tara McNeil <taralynnemcneil@gmail.com>
2016-11-25 23:53:39 -0500

    Automoblox

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(function () {
+    /*
+    - how to call the plugin:
+    $( selector ).cbpFWSlider( [options] );
+    - options:
+    {
+        // default transition speed (ms)
+        speed : 500,
+        // default transition easing
+        easing : 'ease'
+    }
+    - destroy:
+    $( selector ).cbpFWSlider( 'destroy' );
+    */
+
+	$('#cbp-fwslider').cbpFWSlider();
+
+});
